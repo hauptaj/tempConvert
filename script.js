c5b7e0592b25274eec4fbec32d2d320f96d450a0
Andrea Haupt <hauptaj@gmail.com>
2017-02-02 14:23:56 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// window.alert("hi");
+
+var temperatureInF = prompt ("Enter temperature in Farenheit")
+var temperatureInC = (temperatureInF-32) * 5/9;
+console.log ("The temperature is " + temperatureInC + "C");
+
